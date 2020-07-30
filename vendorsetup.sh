d0d582c1651a583b82a74dd53b5a19af8451a8ab
shottard <simonha100@gmail.com>
2020-07-31 00:04:19 +0200

    Updated build variables.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,19 +7,21 @@
         export FOX_ASH_IS_BASH=1
         export FOX_USE_NANO_EDITOR=1
         export FOX_USE_TAR_BINARY=1
-        export FOX_USE_ZIP_BINARY=1
+        export FOX_USE_UNZIP_BINARY=1
         export FOX_REPLACE_BUSYBOX_PS=1
         export FOX_RESET_SETTINGS=1
         export OF_USE_GREEN_LED=1
-        export FOX_VERSION=11
+        export FOX_VERSION=R11
         export OF_MAINTAINER=ShotTard
         export OF_ALLOW_DISABLE_NAVBAR=0
         export OF_CHECK_OVERWRITE_ATTEMPTS=1
 	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
 	export OF_TWRP_COMPATIBILITY_MODE=1
+	export OF_USE_TWRP_SAR_DETECT=1
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
 	export FOX_R11=1
-	export FOX_USE_UNZIP_BINARY=1
 	export OF_FORCE_DISABLE_DM_VERITY_FORCED_ENCRYPTION=1
+	export OF_QUICK_BACKUP_LIST="/boot;/recovery;/data;/system;/vendor;/persist;modem;efs;"
 
 
 add_lunch_combo omni_dumpling-eng
